108dd955a1e20b3d2a3c4a09c036d545d54ea95c
AntonioLemos316 <tonysenha@gmail.com>
2024-03-27 14:38:59 -0300

    update: criação do metodo POST no endpoint /time e criação do schema e model do time com o mongoose

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import conn from './database/mongodb.js'
+import Time from './model/timeModel.js'
 
 const app = express()
 const PORT = 3000
@@ -8,6 +9,21 @@ app.use(express.json())
 
 app.get('/teste', (req, res) => {
     res.send('Olá Mundo!, no endpoint /teste com o metodo GET')
+})
+
+app.post('/time', async (req, res) => {
+    const {nome, nacionalidade, estadio, capacidadeEstadio, isCenturyOld} = req.body
+    try{
+        if(!nome || !nacionalidade || !estadio || !capacidadeEstadio || !isCenturyOld){
+            return res.status(400).send({message: 'Preencha todos os campos'})
+        }
+        const newTime = { nome, nacionalidade, estadio, capacidadeEstadio, isCenturyOld }
+        const time = await Time.create(newTime)
+        return res.status(201).send(time)
+
+    }catch(error){
+        console.log(error)
+    }
 })
 
 conn
